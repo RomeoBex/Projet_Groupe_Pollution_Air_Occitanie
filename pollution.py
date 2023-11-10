18ca4893a142af35a7e24b77741c9b381f6e2c3b
RomeoBex <romeobex1@yahoo.com>
2023-11-10 14:29:03 +0100

    fichier csv sur les principaux polluants + python

--- pollution.py
+++ pollution.py
@@ -1,2 +1,10 @@
 import pandas as pd
-from unidecode import unidecode
+
+# Remplacez 'votre_fichier.csv' par le chemin de votre fichier CSV
+chemin_du_fichier = 'ind_occitanie_2020.csv'
+
+# Chargez le fichier CSV dans un DataFrame en spécifiant l'encodage et le délimiteur
+dataframe = pd.read_csv(chemin_du_fichier, delimiter=';', encoding='ISO-8859-1')
+
+# Affichez les premières lignes du DataFrame pour vérification
+print(dataframe.head())
