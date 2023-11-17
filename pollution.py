d64c76a942ca928dce1557a7b34ad184a39fb8bc
RomeoBex <romeobex1@yahoo.com>
2023-11-17 11:15:52 +0100

    carte

--- pollution.py
+++ pollution.py
@@ -94,3 +94,237 @@ plt.xlabel('Département')
 plt.ylabel('Nombre d\'occurrences')
 plt.xticks(rotation=45, ha='right')  # Rotation des étiquettes sur l'axe x pour une meilleure lisibilité
 plt.show()
+
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Remplacez 'votre_fichier.csv' par le chemin vers votre fichier CSV
+chemin_fichier_csv = 'Mesure_mensuelle_Region_Occitanie_Polluants_Principaux.csv'
+
+# Charger le fichier CSV dans un DataFrame
+df = pd.read_csv(chemin_fichier_csv)
+
+# Créer un nuage de points pour les coordonnées X et Y
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x='X', y='Y', data=df, hue='nom_dept', palette='viridis', s=50)
+plt.title('Nuage de points : Coordonnées X et Y par département')
+plt.xlabel('Coordonnée X')
+plt.ylabel('Coordonnée Y')
+plt.legend(title='Département', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.show()
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Remplacez 'votre_fichier.csv' par le chemin vers votre fichier CSV
+chemin_fichier_csv = 'Mesure_mensuelle_Region_Occitanie_Polluants_Principaux.csv'
+
+# Charger le fichier CSV dans un DataFrame
+df = pd.read_csv(chemin_fichier_csv)
+
+# Compter le nombre d'occurrences de chaque typologie
+typologie_counts = df['typologie'].value_counts()
+
+# Créer un graphique à secteurs
+plt.figure(figsize=(8, 8))
+plt.pie(typologie_counts, labels=typologie_counts.index, autopct='%1.1f%%', startangle=140, colors=sns.color_palette('pastel'))
+plt.title('Répartition des stations par typologie')
+plt.show()
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Remplacez 'votre_fichier.csv' par le chemin vers votre fichier CSV
+chemin_fichier_csv = 'Mesure_mensuelle_Region_Occitanie_Polluants_Principaux.csv'
+
+# Charger le fichier CSV dans un DataFrame
+df = pd.read_csv(chemin_fichier_csv)
+
+# Supprimer les valeurs NaN pour l'histogramme
+valeurs_non_nan = df['valeur'].dropna()
+
+# Créer un histogramme
+plt.figure(figsize=(10, 6))
+sns.histplot(valeurs_non_nan, bins=20, kde=True, color='skyblue')
+plt.title('Distribution des valeurs de concentration')
+plt.xlabel('Concentration')
+plt.ylabel('Nombre d\'occurrences')
+plt.show()
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Charger le fichier CSV dans un DataFrame
+chemin_fichier_csv = 'Mesure_mensuelle_Region_Occitanie_Polluants_Principaux.csv'
+df = pd.read_csv(chemin_fichier_csv)
+
+# Créer une figure avec plusieurs sous-plots
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(15, 10))
+
+# Afficher les 5 premières lignes du DataFrame
+print("5 premières lignes du DataFrame :")
+print(df.head())
+
+# Résumé statistique des variables numériques
+summary_stats = df.describe()
+print("\nRésumé statistique :")
+print(summary_stats)
+
+# Histogramme des valeurs de concentration
+axes[0, 0].hist(df['valeur'].dropna(), bins=20, color='skyblue')
+axes[0, 0].set_title('Distribution des valeurs de concentration')
+axes[0, 0].set_xlabel('Concentration')
+axes[0, 0].set_ylabel('Nombre d\'occurrences')
+
+# Diagramme à barres pour la répartition par département
+sns.countplot(x='nom_dept', data=df, ax=axes[0, 1])
+axes[0, 1].set_title('Répartition des données par département')
+axes[0, 1].set_xlabel('Département')
+axes[0, 1].set_ylabel('Nombre d\'occurrences')
+axes[0, 1].tick_params(axis='x', rotation=45)
+
+# Nuage de points pour les coordonnées X et Y
+sns.scatterplot(x='X', y='Y', data=df, hue='nom_dept', palette='viridis', s=50, ax=axes[1, 0])
+axes[1, 0].set_title('Nuage de points : Coordonnées X et Y par département')
+axes[1, 0].set_xlabel('Coordonnée X')
+axes[1, 0].set_ylabel('Coordonnée Y')
+axes[1, 0].legend(title='Département', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+# Graphique à secteurs pour la répartition par typologie
+typologie_counts = df['typologie'].value_counts()
+axes[1, 1].pie(typologie_counts, labels=typologie_counts.index, autopct='%1.1f%%', startangle=140, colors=sns.color_palette('pastel'))
+axes[1, 1].set_title('Répartition des stations par typologie')
+
+# Ajuster l'espacement entre les sous-plots
+plt.tight_layout()
+
+# Afficher les graphiques
+plt.show()
+
+
+# %%
+
+import pandas as pd
+from ipyleaflet import Map, TileLayer, basemaps, GeoJSON
+
+# Charger le fichier CSV dans un DataFrame
+chemin_fichier_csv = 'Mesure_mensuelle_Region_Occitanie_Polluants_Principaux.csv'
+df = pd.read_csv(chemin_fichier_csv)
+
+# Créer une colonne 'geometry' avec les coordonnées X et Y sous forme de GeoJSON
+df['geometry'] = df.apply(lambda row: {"type": "Point", "coordinates": [row['X'], row['Y']]}, axis=1)
+
+# Créer une carte avec le service WMTS
+carte = Map(center=(43.611015, 3.876733), zoom=9)
+
+# Ajouter une couche WMTS à la carte
+wmts_url = "https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/WMTS"
+wmts_layer = TileLayer(url=wmts_url, name="WMTS Layer")
+carte.add_layer(wmts_layer)
+
+# Créer une GeoJSON FeatureCollection à partir des données de votre DataFrame
+geojson_data = {
+    "type": "FeatureCollection",
+    "features": []
+}
+
+for index, row in df.iterrows():
+    feature = {
+        "type": "Feature",
+        "geometry": row['geometry'],
+        "properties": {"nom_dept": row['nom_dept'], "valeur": row['valeur']}
+    }
+    geojson_data['features'].append(feature)
+
+# Créer une couche GeoJSON pour les zones polluées
+geojson_layer = GeoJSON(data=geojson_data, style={'color': 'red', 'opacity': 0.8, 'weight': 1.5})
+carte.add_layer(geojson_layer)
+
+# Afficher la carte
+carte
+
+
+#pour calculer la valeur seuil 
+# Calculer la moyenne et l'écart type des valeurs de pollution
+moyenne_pollution = df['valeur'].mean()
+ecart_type_pollution = df['valeur'].std()
+
+# Définir le seuil comme la moyenne plus 2 fois l'écart type
+seuil_valeur_elevee = moyenne_pollution + 2 * ecart_type_pollution
+
+print("Moyenne de la pollution:", moyenne_pollution)
+print("Écart type de la pollution:", ecart_type_pollution)
+print("Seuil de valeur élevée:", seuil_valeur_elevee)
+
+
+
+
+# %%
+#Potentiel bon code 
+import pandas as pd
+from ipyleaflet import Map, TileLayer, basemaps, GeoJSON, Marker
+import ipywidgets as widgets
+from IPython.display import display
+
+# Charger le fichier CSV dans un DataFrame
+chemin_fichier_csv = 'Mesure_mensuelle_Region_Occitanie_Polluants_Principaux.csv'
+df = pd.read_csv(chemin_fichier_csv)
+
+# Créer une colonne 'geometry' avec les coordonnées X et Y sous forme de GeoJSON
+df['geometry'] = df.apply(lambda row: {"type": "Point", "coordinates": [row['X'], row['Y']]}, axis=1)
+
+# Créer une carte avec le service WMTS
+carte = Map(center=(43.611015, 3.876733), zoom=9)
+
+# Ajouter une couche WMTS à la carte
+wmts_url = "https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/WMTS"
+wmts_layer = TileLayer(url=wmts_url, name="WMTS Layer")
+carte.add_layer(wmts_layer)
+
+# Créer une GeoJSON FeatureCollection à partir des données de votre DataFrame
+geojson_data = {
+    "type": "FeatureCollection",
+    "features": []
+}
+
+# Marqueurs pour les valeurs élevées
+high_value_markers = []
+
+for index, row in df.iterrows():
+    feature = {
+        "type": "Feature",
+        "geometry": row['geometry'],
+        "properties": {"nom_dept": row['nom_dept'], "valeur": row['valeur']}
+    }
+    geojson_data['features'].append(feature)
+
+    # Ajouter un marqueur si la valeur de pollution est élevée
+    if row['valeur'] > SEUIL_DE_VALEUR_ELEVEE:
+        marker = Marker(location=(row['Y'], row['X']), draggable=False, title=f"Valeur: {row['valeur']}")
+        high_value_markers.append(marker)
+
+# Créer une couche GeoJSON pour les zones polluées
+geojson_layer = GeoJSON(data=geojson_data, style={'color': 'red', 'opacity': 0.8, 'weight': 1.5})
+carte.add_layer(geojson_layer)
+
+# Ajouter les marqueurs à la carte
+for marker in high_value_markers:
+    carte.add_layer(marker)
+
+# Créer une légende
+legend = widgets.VBox([
+    widgets.HTML(value="<b>Légende</b>"),
+    widgets.HTML(value='<div style="background-color: red; width: 20px; height: 20px; border: 1px solid black; display: inline-block;"></div> Valeur élevée'),
+    # Ajoutez d'autres lignes ci-dessus pour d'autres valeurs de pollution
+])
+
+# Afficher la carte avec la légende
+display(widgets.HBox([carte, legend]))
