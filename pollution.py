a725c349c24ec75eefb0a20860fcac146f14ebee
RomeoBex <romeobex1@yahoo.com>
2023-11-17 10:43:13 +0100

    graph

--- pollution.py
+++ pollution.py
@@ -57,3 +57,40 @@ plt.savefig(chemin_enregistrement, format='svg')
 plt.show()
 
 # %%
+
+#Carte interactive 
+from ipyleaflet import Map, TileLayer, basemaps
+
+# Remplacez l'URL par le service WMTS que vous souhaitez utiliser
+wmts_url = "https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/WMTS"
+
+# Créer une carte avec le service WMTS
+carte = Map(center=(43.611015, 3.876733), zoom=12)
+
+# Ajouter une couche WMTS à la carte
+wmts_layer = TileLayer(url=wmts_url, name="WMTS Layer")
+carte.add_layer(wmts_layer)
+
+# Afficher la carte
+carte
+
+# %%
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Remplacez 'votre_fichier.csv' par le chemin vers votre fichier CSV
+chemin_fichier_csv = 'Mesure_mensuelle_Region_Occitanie_Polluants_Principaux.csv'
+
+# Charger le fichier CSV dans un DataFrame
+df = pd.read_csv(chemin_fichier_csv)
+
+# Créer un diagramme à barres pour la variable 'nom_dept'
+plt.figure(figsize=(12, 6))
+sns.countplot(x='nom_dept', data=df)
+plt.title('Répartition des données par département')
+plt.xlabel('Département')
+plt.ylabel('Nombre d\'occurrences')
+plt.xticks(rotation=45, ha='right')  # Rotation des étiquettes sur l'axe x pour une meilleure lisibilité
+plt.show()
