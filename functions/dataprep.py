0c4dc2f26ac6ce2401594f71f94d60a54f576e5c
Linus Marheineke <linus.marheineke@etu.umontpellier.fr>
2023-11-26 17:02:27 +0100

    data preperation and visualization

--- functions/dataprep.py
+++ functions/dataprep.py
@@ -2,10 +2,54 @@ import requests
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime as dt
 
 
-def get_data():
-    tj = "https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/Mesure_horaire_(30j)_Region_Occitanie_Polluants_Reglementaires_1/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-    mensuelle = "https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_mensuelle_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-    response = requests.get(mensuelle)
+def get_data(time):
+    if time == "tj":
+        url = "https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/Mesure_horaire_(30j)_Region_Occitanie_Polluants_Reglementaires_1/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+    elif time == "m":
+        url = "https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_mensuelle_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+    response = requests.get(url)
     return response.json()
+
+
+def extract_data_to_df(data: dict) -> pd.DataFrame:
+    """extracts important data from dict and returns dataframe with data"""
+
+    # get important data
+    cities = []
+    starts = []
+    ends = []
+    val = []
+    poll_ue = []
+    typo = []
+    infl = []
+    nom_poll = []
+
+    for element in data["features"]:
+        cities += [element.get("attributes").get("nom_com")]
+        starts += [element.get("attributes").get("date_debut")]
+        ends += [element.get("attributes").get("date_fin")]
+        val += [element.get("attributes").get("valeur")]
+        poll_ue += [element.get("attributes").get("id_poll_ue")]
+        typo += [element.get("attributes").get("typologie")]
+        infl += [element.get("attributes").get("influence")]
+        nom_poll += [element.get("attributes").get("nom_poll")]
+
+    # construct df from data
+    df_data = pd.DataFrame()
+    df_data["city"] = cities
+    df_data["start"] = starts
+    df_data["end"] = ends
+    df_data["values"] = val
+    df_data["poll_ue"] = poll_ue
+    df_data["nom_poll"] = nom_poll
+    df_data["influence"] = infl
+    df_data["typology"] = typo
+
+    # convert timestamps to dates
+    df_data["start"] = df_data["start"].apply(lambda x: dt.fromtimestamp(x / 1e3))
+    df_data["end"] = df_data["end"].apply(lambda x: dt.fromtimestamp(x / 1e3))
+
+    return df_data
---
+++ functions/visuals.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime as dt
+
+
+def visualize_df(df, city, value, nom_poll):
+    """visualize the dataframe"""
+
+    # Filter and sort DataFrame by timestamp
+    viz_df = df[(df["city"] == city) & (df["nom_poll"] == nom_poll)].sort_values(
+        by="start"
+    )
+    # Handle double values at same date
+    viz_df = viz_df.groupby('start')['values'].mean().reset_index()
+
+    # Extract the timestamp and value columns
+    timestamps = viz_df["start"]
+    values = viz_df[value]
+
+    # Plot the time series
+    plt.figure(figsize=(10, 6))
+    plt.plot(timestamps, values, marker="o", linestyle="-")
+    plt.title(f"Time Series {city}")
+    plt.xlabel("Timestamp")
+    plt.ylabel(f"{value}")
+    plt.grid(True)
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
