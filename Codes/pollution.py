f0323316893a86a47a49c0d963d0eaf2c1ba2767
RomeoBex <romeobex1@yahoo.com>
2023-12-07 14:36:05 +0100

    final works 07/12/2023

--- Codes/pollution.py
+++ Codes/pollution.py
@@ -878,3 +878,27 @@ carte_occitanie
 
 
 # %%
+#graph qui bouge 
+import plotly.express as px
+from plotly.offline import plot
+
+df = px.data.gapminder()
+fig = px.scatter(
+    df,
+    x="gdpPercap",
+    y="lifeExp",
+    animation_frame="year",
+    animation_group="country",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=55,
+    range_x=[100, 100000],
+    range_y=[25, 90],
+)
+fig.show("notebook")
+
+# %%
+Mesure_annuelle_Region_Occitanie_Polluants_Principaux.csv
+
